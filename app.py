807c0e8e04cda8e1db7b6c4cb8e13bd9ca390044
Sayuri Yoshimatsu <cotocotocotton66@gmail.com>
2020-08-06 21:12:39 +0900

    テンプレートの使い方

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask ,render_template
 
 app = Flask(__name__)
 
@@ -11,7 +11,10 @@ def helloWorld():
 def greet(name):
     return name + "さん！" + "ハローー！"
 
-
+@app.route("/template")
+def template():
+    py_name = "ゴースト"
+    return render_template("index.html",name = py_name)
 
 if __name__ == "__main__":
     app.run(debug=True)
